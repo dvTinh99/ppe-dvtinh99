3bc78102dfffc6f864c3823e1f80f10924b1db44
dvTinh99 <tinh5969@gmail.com>
2021-05-13 19:53:56 +0700

    add ppe/dvtinh

---
+++ src/Providers/CoreDBServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+namespace ppe\dvtinh\Providers;
+use Illuminate\Support\ServiceProvider;
+class CoreDBServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+//        dd('Ok');
+    }
+
+}
