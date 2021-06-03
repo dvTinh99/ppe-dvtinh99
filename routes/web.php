939bd0c6900713515286dd233022cf39ef8efbd4
dvTinh99 <66030457+dvTinh99@users.noreply.github.com>
2021-06-03 11:05:06 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,8 @@ Route::group(['prefix' => 'ppe-core'], function() {
 Route::get('test-jenkin', function(){
 	dd("ok nhes");
 });
+
+Route::get('test-jenkin2', function(){
+	dd("ok nhe");
+});
 Route::post("/register", [\ppe\dvtinh\Http\Controllers\AuthController::class, 'register']);
